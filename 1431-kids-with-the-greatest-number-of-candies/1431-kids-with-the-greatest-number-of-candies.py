6cdb601df4a59a45e841b8a73a61e74de9ed126d
Muhammad Haris <142180841+HarisMuhammadAwan@users.noreply.github.com>
2025-12-27 03:24:24 +0500

    Time: 0 ms (100%), Space: 17.3 MB (95.97%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.py
@@ -1,12 +1,8 @@
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        # 1. Find max (O(N) time, O(1) space)
-        max_candy = max(candies)
-        
-        # 2. Overwrite the input list directly
-        for i in range(len(candies)):
-            # Replace the integer at index 'i' with a boolean
-            candies[i] = (candies[i] + extraCandies >= max_candy)
-            
-        # 3. Return the modified input list
-        return candies
+        result = []
+        max_candy = max(candies)  
+        for x in candies:
+            temp = x + extraCandies
+            result.append(temp >= max_candy)
+        return result
